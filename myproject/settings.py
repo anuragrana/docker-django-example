04ffc2be8d633994013402d79303f6a498301b38
Anurag Rana <anuragrana31189@gmail.com>
2018-02-15 18:32:37 +0530

    added docker-compose file

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'mydb',
+        'PORT': '3306',
     }
 }
 
